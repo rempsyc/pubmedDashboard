c5098d11725612575cea29e1e676e2b4d9e71db9
Rémi Thériault <13123390+rempsyc@users.noreply.github.com>
2023-09-10 18:32:01 -0400

    table_country_year

--- R/scatter_country_year.R
+++ R/scatter_country_year.R
@@ -27,17 +27,6 @@
 #' @export
 
 scatter_country_year <- function(data, method = "lm") {
-  df_country_year_papers <- data %>%
-    dplyr::filter(!is.na(.data$country)) %>%
-    dplyr::count(.data$year, name = "Papers") %>%
-    dplyr::arrange(dplyr::desc(.data$year), dplyr::desc(.data$Papers))
-
-  get_year_papers <- function(year) {
-    df_country_year_papers[which(
-      df_country_year_papers$year == year
-    ), "Papers"]
-  }
-
   df_country_year_missing <- data %>%
     dplyr::filter(is.na(.data$country)) %>%
     dplyr::group_by(.data$year) %>%
@@ -57,12 +46,8 @@ scatter_country_year <- function(data, method = "lm") {
     dplyr::group_by(.data$year, .data$country) %>%
     dplyr::filter(!is.na(.data$continent)) %>%
     dplyr::count(name = "Papers") %>%
-    dplyr::mutate(percentage = as.numeric(round(.data$Papers / get_year_papers(.data$year) * 100, 2)))
-
-  # df_country_year2 <- df_country_year %>%
-  #   dplyr::ungroup() %>%
-  #   dplyr::add_row(df_country_year_missing) %>%
-  #   dplyr::arrange(dplyr::desc(.data$year), dplyr::desc(.data$Papers))
+    dplyr::mutate(percentage = as.numeric(round(.data$Papers / get_year_papers(
+      data, .data$year) * 100, 2)))
 
   getPalette <- grDevices::colorRampPalette(RColorBrewer::brewer.pal(8, "Set2"))
   colours.country2 <- getPalette(length(unique(df_country_year$country)))
@@ -82,4 +67,16 @@ scatter_country_year <- function(data, method = "lm") {
       ytitle = "% of All Papers"
     ) %>%
     plotly::ggplotly(tooltip = c("x", "y"))
+}
+
+#' @noRd
+get_year_papers <- function(data, year) {
+  df_country_year_papers <- data %>%
+    dplyr::filter(!is.na(.data$country)) %>%
+    dplyr::count(.data$year, name = "Papers") %>%
+    dplyr::arrange(dplyr::desc(.data$year), dplyr::desc(.data$Papers))
+
+  df_country_year_papers[which(
+    df_country_year_papers$year == year
+  ), "Papers"]
 }
---
+++ R/table_country_year.R
@@ -0,0 +1,64 @@
+#' @title Generate table of journal paper percentages, by country and year
+#' @param data The processed dataframe of data
+#' @param datatable Whether to output a [DT::datatable] HTML table widget
+#'  instead of a regular dataframe (defaults to TRUE).
+#' @examples
+#' \dontshow{
+#' .old_wd <- setwd(tempdir())
+#' }
+#' \dontrun{
+#' pubmed_query_string <- paste(
+#'   "passion [Title/Abstract]",
+#'   "AND Dualistic Model of Passion [Text Word]"
+#' )
+#'
+#' save_process_pubmed_batch(
+#'   pubmed_query_string,
+#'   year_low = 2023,
+#'   year_high = 2023
+#' )
+#' data <- read_bind_all_data()
+#' table_country_year(data)
+#' }
+#' \dontshow{
+#' unlink("easyPubMed_data_01.txt")
+#' setwd(.old_wd)
+#' }
+#' @importFrom rlang .data
+#' @export
+
+table_country_year <- function(data, datatable = TRUE) {
+  x <- data %>%
+    dplyr::group_by(.data$year, .data$country) %>%
+    dplyr::filter(!is.na(.data$continent)) %>%
+    dplyr::count(name = "Papers") %>%
+    dplyr::mutate(percentage = as.numeric(round(.data$Papers / get_year_papers(data, .data$year) * 100, 2)))
+
+  df_country_year_missing <- data %>%
+    dplyr::filter(is.na(.data$country)) %>%
+    dplyr::group_by(.data$year) %>%
+    dplyr::count(.data$year, name = "Papers") %>%
+    dplyr::arrange(dplyr::desc(.data$year), dplyr::desc(.data$Papers)) %>%
+    dplyr::left_join(by = "year", data %>%
+                       dplyr::group_by(.data$year) %>%
+                       dplyr::count(.data$year, name = "all_papers") %>%
+                       dplyr::arrange(dplyr::desc(.data$year))) %>%
+    dplyr::mutate(
+      percentage = round(.data$Papers / .data$all_papers * 100, 2),
+      country = "Missing*"
+    ) %>%
+    dplyr::select(-"all_papers")
+
+  x <- x %>%
+    dplyr::ungroup() %>%
+    dplyr::add_row(df_country_year_missing) %>%
+    dplyr::arrange(dplyr::desc(.data$year), dplyr::desc(.data$Papers))
+
+  if (isTRUE(datatable)) {
+    x <- DT::datatable(x,
+      options = list(searching = FALSE, paging = FALSE),
+      caption = "Journal paper percentages, by country and year"
+    )
+  }
+  x
+}
